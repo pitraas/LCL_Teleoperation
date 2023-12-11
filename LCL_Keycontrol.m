8517ae86e6ef6399484eaaa1a2a6f5348b7d96bb
pitraas <pit.raas@gmail.com>
2023-12-11 16:46:16 +0100

    renamed some variables

--- LCL_Keycontrol.m
+++ LCL_Keycontrol.m
@@ -2,54 +2,60 @@ clear keyEntered pattern posMaster posMasterString posMasterNum
 clear programmRunning r SendCommand stepSize
 pattern = digitsPattern;
 r = rateControl(1/20);
+%Stepsize encodersteps
 stepSize = 100;
 
-% Get iitial Position
+% Get initial Position
 RobMaster.writeline('read');
+
+
 
 posMasterString = RobMaster.readline();
 posMaster = extract(posMasterString, pattern);
-posMasterNum(1) = str2num(posMaster(1));
-posMasterNum(2) = str2num(posMaster(3));
-posMasterNum(3) = str2num(posMaster(5));
-posMasterNum(4) = str2num(posMaster(7));
-posMasterNum(5) = str2num(posMaster(9));
+
+
+robot(1).Jointposition = str2num(posMaster(1));
+robot(2).Jointposition = str2num(posMaster(3));
+robot(3).Jointposition = str2num(posMaster(5));
+robot(4).Jointposition = str2num(posMaster(7));
+robot(5).Jointposition = str2num(posMaster(9));
 % waitforbuttonpress;
 % close(figure(1));
 
-programmRunning = 1;
 
+programmRunning = 1;
 while programmRunning 
     
     keyEntered  = getkey();
     switch(keyEntered)
         case '1'
-            posMasterNum(1) = posMasterNum(1) + stepSize;
+            robot(1).Jointposition = robot(1).Jointposition + stepSize;
         case 'q'
-            posMasterNum(1) = posMasterNum(1) - stepSize;
+            robot(1).Jointposition = robot(1).Jointposition - stepSize;
         case '2'
-            posMasterNum(2) = posMasterNum(2) + stepSize;
+            robot(2).Jointposition = robot(2).Jointposition + stepSize;
         case 'w'
-            posMasterNum(2) = posMasterNum(2) - stepSize;
+            robot(2).Jointposition = robot(2).Jointposition - stepSize;
         case '3'
-            posMasterNum(3) = posMasterNum(3) + stepSize;
+            robot(3).Jointposition = robot(3).Jointposition + stepSize;
         case 'e'
-            posMasterNum(3) = posMasterNum(3) - stepSize;
+            robot(3).Jointposition = robot(3).Jointposition - stepSize;
         case '4'
-            posMasterNum(4) = posMasterNum(4) + stepSize;
+            robot(4).Jointposition = robot(4).Jointposition + stepSize;
         case 'r'
-            posMasterNum(4) = posMasterNum(4) - stepSize;
+            robot(4).Jointposition = robot(4).Jointposition - stepSize;
         case '5'
-            posMasterNum(5) = posMasterNum(5) + stepSize;
+            robot(5).Jointposition = robot(5).Jointposition + stepSize;
         case 't'
-            posMasterNum(5) = posMasterNum(5) - stepSize;
+            robot(5).Jointposition = robot(5).Jointposition - stepSize;
         case 'c'
             programmRunning = 0;
     end
-  
-    SendCommand = append('move ',num2str(posMasterNum(1)),' ',...
-        num2str(posMasterNum(2)),' ',num2str(posMasterNum(3)),' ',...
-        num2str(posMasterNum(4)),' ',num2str(posMasterNum(5)), ' D=0');
+
+    % Send new Position to LCL
+    SendCommand = append('move ',num2str(robot(1).Jointposition),' ',...
+        num2str(robot(2).Jointposition),' ',num2str(robot(3).Jointposition),' ',...
+        num2str(robot(4).Jointposition),' ',num2str(robot(5).Jointposition), ' D=0');
     disp(SendCommand);
     RobMaster.writeline(SendCommand);
     % waitfor(r);
