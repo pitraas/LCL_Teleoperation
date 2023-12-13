35758a4ad452e2c39d8979ce115843cee81380a1
pitraas <pit.raas@gmail.com>
2023-12-13 15:26:29 +0100

    Changed initial position, expecting to get better inverseKinematics / near the realistic starting Point

--- LCL_Keycontrol_Model.m
+++ LCL_Keycontrol_Model.m
@@ -1,9 +1,9 @@
 clc
 
+
 % Define variables
 r = rateControl(10);
-stepSize = 0.02
-q; %Stepsize in cm
+stepSize = 0.02; %Stepsize in cm
 homePose = homeConfiguration(LCL_Tree);
 ik = inverseKinematics('RigidBodyTree', LCL_Tree); % create inverseKinematics Object
 weights = [0.25 0.25 0.25 1 1 1]; %specify weights for orientation / Position
@@ -12,7 +12,19 @@ weights = [0.25 0.25 0.25 1 1 1]; %specify weights for orientation / Position
 
 % Move to initial Position 
 
-RobMaster.writeline('move 2048 2048 2048 2048 2048 T=1000 D=0');
+%RobMaster.writeline('move 2048 2048 2048 2048 2048 T=1000 D=0');
+RobMaster.writeline('move 2048 1661 420 3200 2048 T=1000 D=0');
+%oldEncoderValues = [2048; 2048; 2048; 2048; 2048];
+oldEncoderValues = [2048; 1661; 420; 3200; 2048];
+
+% set new HomePose to current pose
+RobMaster.writeline('enable');
+pause(1)
+currPoseEncoder = LCL_getCurrentPose(RobMaster);
+currPoseRadian = LCL_convertEncoder2Radian(currPoseEncoder);
+for i = 1:length(currPoseRadian)
+    homePose(i).JointPosition = currPoseRadian(i);
+end
 
 oldPose = homePose;
 
@@ -55,20 +67,29 @@ while programmRunning
     % Construct Command
     SendCommand = append('move ',num2str(configEncodervalues(1)),' ',...
         num2str(configEncodervalues(2)),' ',num2str(configEncodervalues(3)),' ',...
-        num2str(configEncodervalues(4)),' ',num2str(configEncodervalues(5)), ' T=1000 D=0');
+        num2str(configEncodervalues(4)),' ',num2str(configEncodervalues(5)), ' T=500 D=0');
     
     % Display comand
     disp(['Command: ', SendCommand]);
 
-    % check for limits
-
-    
-    % Ask if movement should be executed
-    disp('Exceute Movement? Yes: enter, No: n')
-    key = getkey();
-    if key == 13
-        RobMaster.writeline(SendCommand);
+    % check that new configuration is near the old one
+    outOfLimit = 0;
+    for i = 1:length(configEncodervalues)
+        if (abs(configEncodervalues(i) - oldEncoderValues(i)) > 500)
+            outOfLimit = 1;
+            error('New configuration out of Limits!');
+        end
     end
+
+    if (outOfLimit == 0)    
+        % disp('Exceute Movement? Yes: enter, No: n')
+        % key = getkey();
+        % if key == 13
+        RobMaster.writeline(SendCommand);
+        % end
+        oldEncoderValues = configEncodervalues;
+    end
+    
     waitfor(r);
 end
 
