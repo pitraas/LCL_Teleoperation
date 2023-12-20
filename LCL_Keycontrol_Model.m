a16429c34afb91481e1f08befecad9591151eb22
pitraas <pit.raas@gmail.com>
2023-12-20 13:28:55 +0100

    restructured and commented code

--- LCL_Keycontrol_Model.m
+++ LCL_Keycontrol_Model.m
@@ -1,23 +1,25 @@
 clc
 
 
-% Define variables
+%% *** Define variables ***
+
 r = rateControl(10);
 stepSize = 0.02; %Stepsize in cm
 homePose = homeConfiguration(LCL_Tree);
 ik = inverseKinematics('RigidBodyTree', LCL_Tree); % create inverseKinematics Object
 weights = [0.25 0.25 0.25 1 1 1]; %specify weights for orientation / Position
+nbJoints = 5;
+minLimit = [500; 1000; 300; 1000; 1500]; % robot joint min limits
+maxLimit = [3596; 3000; 3800; 3000; 3500]; % robot joint max limits
 
+%% *** Move robot to starting Position ***
 
-
-% Move to initial Position 
-
-%RobMaster.writeline('move 2048 2048 2048 2048 2048 T=1000 D=0');
-RobMaster.writeline('move 2048 1661 420 3200 2048 T=1000 D=0');
+%RobMaster.writeline('move 2048 2048 2048 2048 2048 T=1000 D=0'); % stretched Position
 %oldEncoderValues = [2048; 2048; 2048; 2048; 2048];
+RobMaster.writeline('move 2048 1661 420 3200 2048 T=1000 D=0'); % Sitting Human Position 
 oldEncoderValues = [2048; 1661; 420; 3200; 2048];
 
-% set new HomePose to current pose
+%% *** set new rigidbodytree HomePose to current pose ***
 RobMaster.writeline('enable');
 pause(1)
 currPoseEncoder = LCL_getCurrentPose(RobMaster);
@@ -26,9 +28,19 @@ for i = 1:length(currPoseRadian)
     homePose(i).JointPosition = currPoseRadian(i);
 end
 
-oldPose = homePose;
+oldPose = homePose; %intitialization of oldPose
+nextPose = getTransform(LCL_Tree,homePose,'Axis_5_Camera','base'); % Initialize nextPose to be a Transform Matrix
 
-nextPose = getTransform(LCL_Tree,homePose,'Axis_5_Camera','base');
+
+%% *** Main Loop: ... ***
+% Read user entry (new task space Pose),
+% Calculate new joint positions
+% convert joint position from rad to encoder values
+% construct command message to be sent to robot
+% check robot limits
+% check how different the next position is from the current one
+% Show new config in plot (rigidbodytree)
+% -> send command to robot if limits are ok
 
 disp('Change Pose: x+: s, x-: x, y+: a, y-: y, z+: d, z-: c, quit: q');
 programmRunning = 1;
@@ -53,26 +65,32 @@ while programmRunning
     end
     
     disp(nextPose(1:3,4));
+
     % Calculate the joint positions using the inverseKinematics object.
     initialguess = oldPose;
     [configSolutionRadian,solnInfo] = ik("Axis_5_Camera",nextPose,weights,oldPose);
     oldPose = configSolutionRadian;
 
-    % Show new Position of Model in figure
-    show(LCL_Tree,configSolutionRadian);
-
     % Convert calculated Pose to Encoder values which can be sent to LCL
     configEncodervalues = LCL_convertRadian2Encoder(configSolutionRadian);
     
-    % Construct Command
+    % Construct Command Message
     SendCommand = append('move ',num2str(configEncodervalues(1)),' ',...
         num2str(configEncodervalues(2)),' ',num2str(configEncodervalues(3)),' ',...
-        num2str(configEncodervalues(4)),' ',num2str(configEncodervalues(5)), ' T=500 D=0');
+        num2str(configEncodervalues(4)),' ',num2str(configEncodervalues(5)), ' T=1000 D=0');
     
-    % Display comand
+    % Display command message
     disp(['Command: ', SendCommand]);
 
-    % check that new configuration is near the old one
+    % Check Limits
+    for i = 1:nbJoints
+        if (jointPositionsEncoderMapping(i) < minLimit(i)) || (jointPositionsEncoderMapping(i) > maxLimit(i))
+            msg = ['Joint ', int2str(i), ' out of Limits!'];
+            disp (msg);
+        end
+    end
+
+    %%  check that new configuration is near the old one
     outOfLimit = 0;
     for i = 1:length(configEncodervalues)
         if (abs(configEncodervalues(i) - oldEncoderValues(i)) > 500)
@@ -80,6 +98,9 @@ while programmRunning
             error('New configuration out of Limits!');
         end
     end
+
+    % Show new Position of Model in figure
+    show(LCL_Tree,configSolutionRadian);
 
     if (outOfLimit == 0)    
         % disp('Exceute Movement? Yes: enter, No: n')
@@ -89,6 +110,8 @@ while programmRunning
         % end
         oldEncoderValues = configEncodervalues;
     end
+
+
     
     waitfor(r);
 end
