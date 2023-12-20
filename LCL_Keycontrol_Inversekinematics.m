705c74151bb5a8f2fd6de090f2bf172f70b6be10
pitraas <pit.raas@gmail.com>
2023-12-20 15:19:56 +0100

    update

--- LCL_Keycontrol_Inversekinematics.m
+++ LCL_Keycontrol_Inversekinematics.m
@@ -1,25 +1,27 @@
 clc
 
-
 %% *** Define variables ***
 
-r = rateControl(10);
+r = rateControl(2);
 stepSize = 0.02; %Stepsize in cm
 homePose = homeConfiguration(LCL_Tree);
 ik = inverseKinematics('RigidBodyTree', LCL_Tree); % create inverseKinematics Object
 weights = [0.25 0.25 0.25 1 1 1]; %specify weights for orientation / Position
 nbJoints = 5;
-minLimit = [500; 1000; 300; 1000; 1500]; % robot joint min limits
-maxLimit = [3596; 3000; 3800; 3000; 3500]; % robot joint max limits
+minLimit = [600; 1000; 200; 1900; 1000]; % robot joint min limits
+maxLimit = [3495; 3200; 3700; 3800; 3000]; % robot joint max limits
+
 
 %% *** Move robot to starting Position ***
 
 %RobMaster.writeline('move 2048 2048 2048 2048 2048 T=1000 D=0'); % stretched Position
 %oldEncoderValues = [2048; 2048; 2048; 2048; 2048];
-RobMaster.writeline('move 2048 1661 420 3200 2048 T=1000 D=0'); % Sitting Human Position 
-oldEncoderValues = [2048; 1661; 420; 3200; 2048];
+RobMaster.writeline('move 2048 1661 420 3300 2048 T=1000 D=0'); % Sitting Human Position 
+oldEncoderValues = [2048; 1661; 420; 3300; 2048];
+
 
 %% *** set new rigidbodytree HomePose to current pose ***
+
 RobMaster.writeline('enable');
 pause(1)
 currPoseEncoder = LCL_getCurrentPose(RobMaster);
@@ -62,57 +64,66 @@ while programmRunning
             nextPose(3,4) = nextPose(3,4) - stepSize;
         case 'q'
             programmRunning = 0;
+            break
     end
     
     disp(nextPose(1:3,4));
 
     % Calculate the joint positions using the inverseKinematics object.
     initialguess = oldPose;
-    [configSolutionRadian,solnInfo] = ik("Axis_5_Camera",nextPose,weights,oldPose);
-    oldPose = configSolutionRadian;
-
+    [configSolutionRadian,solnInfo] = ik("Axis_5_Camera",nextPose,weights,initialguess);
+    
     % Convert calculated Pose to Encoder values which can be sent to LCL
     configEncodervalues = LCL_convertRadian2Encoder(configSolutionRadian);
     
     % Construct Command Message
     SendCommand = append('move ',num2str(configEncodervalues(1)),' ',...
         num2str(configEncodervalues(2)),' ',num2str(configEncodervalues(3)),' ',...
-        num2str(configEncodervalues(4)),' ',num2str(configEncodervalues(5)), ' T=1000 D=0');
+        num2str(configEncodervalues(4)),' ',num2str(configEncodervalues(5)), ' T=300 D=0');
     
     % Display command message
     disp(['Command: ', SendCommand]);
 
+    limitflag = 0;
     % Check Limits
     for i = 1:nbJoints
-        if (jointPositionsEncoderMapping(i) < minLimit(i)) || (jointPositionsEncoderMapping(i) > maxLimit(i))
-            msg = ['Joint ', int2str(i), ' out of Limits!'];
-            disp (msg);
+        if (configEncodervalues(i) < minLimit(i)) || (configEncodervalues(i) > maxLimit(i))
+            msg = ['Joint ', int2str(i), ' out of Limits! Try different Pose.'];
+            detailedmsg = [int2str(configEncodervalues(i)),' is not inside [',int2str(minLimit(i)),' ',int2str(maxLimit(i))];
+            disp(msg);
+            disp(detailedmsg);
+            limitflag = 1;
         end
     end
 
     %%  check that new configuration is near the old one
-    outOfLimit = 0;
+    farawayPoseFlag = 0;
     for i = 1:length(configEncodervalues)
         if (abs(configEncodervalues(i) - oldEncoderValues(i)) > 500)
-            outOfLimit = 1;
-            error('New configuration out of Limits!');
+            farawayPoseFlag = 1;
+            msg2 = 'New config to far from old one! Try different Pose.';
+            disp(msg2)
         end
     end
 
     % Show new Position of Model in figure
     show(LCL_Tree,configSolutionRadian);
 
-    if (outOfLimit == 0)    
-        % disp('Exceute Movement? Yes: enter, No: n')
-        % key = getkey();
-        % if key == 13
-        RobMaster.writeline(SendCommand);
-        % end
-        oldEncoderValues = configEncodervalues;
+    if (limitflag == 0 && farawayPoseFlag == 0)    
+        disp('Exceute Movement? Yes: enter, No: n')
+        key = getkey();
+        if key == 13
+            RobMaster.writeline(SendCommand);
+            oldEncoderValues = configEncodervalues;
+            oldPose = configSolutionRadian;
+        end
+        if key == 'q'
+            break
+        end
     end
-
-
-    
     waitfor(r);
 end
+
+RobMaster.writeline('enable');
+
 
