fd5a92843a92dfbdc18489ca042dade1118186a1
pitraas <pit.raas@gmail.com>
2023-12-20 13:30:39 +0100

    changed to connect only one robot

--- Connect_Robots.m
+++ Connect_Robots.m
@@ -2,6 +2,6 @@
 com_port_Master = 'COM5';
 com_port_Slave = 'COM6';
 
-RobMaster = LCLserialConnect(com_port_Master)
-pause(10);
-RobSlave = LCLserialConnect(com_port_Slave)
+RobMaster = LCLserialConnect(com_port_Master);
+%pause(10);
+%RobSlave = LCLserialConnect(com_port_Slave)
